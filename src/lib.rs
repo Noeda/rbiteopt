c08243d2f2cb35a803dcd7f271831e5988b5dbf8
Mikko Juola <mikjuo@gmail.com>
2022-08-08 18:19:08 -0700

    Make it possible to run many copies of BiteOpt optimizer conveniently.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod raw;
 
 use libc::{c_double, c_int, c_void};
 use rcmaes::Vectorizable;
+use std::sync::{Arc, RwLock};
 
 #[derive(Clone, Copy, PartialEq, PartialOrd, Debug)]
 pub struct BiteOptParameters {
@@ -10,6 +11,8 @@ pub struct BiteOptParameters {
     attc: i32,
     lower_bound: f64,
     upper_bound: f64,
+    portfolio_copies: usize, // If greater than 1, makes threads where each thread runs one copy of
+                             // the optimizer.
 }
 
 impl BiteOptParameters {
@@ -31,6 +34,14 @@ impl BiteOptParameters {
 
     pub fn set_upper_bound(&mut self, upper_bound: f64) {
         self.upper_bound = upper_bound;
+    }
+
+    pub fn portfolio_copies(&self) -> usize {
+        self.portfolio_copies
+    }
+
+    pub fn set_portfolio_copies(&mut self, portfolio_copies: usize) {
+        self.portfolio_copies = portfolio_copies;
     }
 
     pub fn iter(&self) -> i32 {
@@ -66,6 +77,7 @@ impl Default for BiteOptParameters {
             attc: 10,
             lower_bound: -1.0,
             upper_bound: 1.0,
+            portfolio_copies: 1,
         }
     }
 }
@@ -76,41 +88,72 @@ struct Userdata<'a> {
 
 pub fn optimize<T, F>(archetype: &T, params: BiteOptParameters, evaluate: F) -> T
 where
-    T: Vectorizable + Clone,
-    F: Fn(T) -> f64,
+    T: Vectorizable + Clone + Send + Sync,
+    <T as Vectorizable>::Context: Sync + Send,
+    F: Fn(T) -> f64 + Sync + Send,
 {
+    if params.portfolio_copies < 1 {
+        panic!("rbiteopt::optimize: params.portfolio_copies must be 1 or greater.");
+    }
+
     let (archetype_vec, archetype_ctx) = archetype.to_vec();
     let dimension = archetype_vec.len() as i32;
 
-    let mut out: Vec<f64> = vec![0.0; dimension as usize];
-    let out_ptr: *mut f64 = out.as_mut_ptr();
+    let archetype_score = evaluate(archetype.clone());
 
-    let raw_evaluate = |vec: &[f64]| -> f64 {
-        let materialized_entity = T::from_vec(vec, &archetype_ctx);
-        evaluate(materialized_entity)
-    };
+    let best: Arc<RwLock<(f64, T)>> = Arc::new(RwLock::new((archetype_score, archetype.clone())));
 
-    let userdata = Userdata {
-        evaluate: Box::new(raw_evaluate),
-    };
+    crossbeam::scope(|s| {
+        let raw_evaluate = |vec: &[f64]| -> f64 {
+            let materialized_entity = T::from_vec(vec, &archetype_ctx);
+            let score = evaluate(materialized_entity.clone());
+            {
+                let mut bestr = best.write().unwrap();
+                if score < bestr.0 {
+                    bestr.0 = score;
+                    bestr.1 = materialized_entity;
+                }
+            }
+            score
+        };
 
-    let userdata_ptr: *const Userdata = &userdata as *const Userdata;
+        let mut threads = vec![];
+        for _ in 0..params.portfolio_copies {
+            let t = s.spawn(move |_| {
+                let mut out: Vec<f64> = vec![0.0; dimension as usize];
+                let out_ptr: *mut f64 = out.as_mut_ptr();
 
-    unsafe {
-        raw::biteopt_optimize(
-            out_ptr,
-            dimension as c_int,
-            userdata_ptr as *const c_void,
-            params.lower_bound as c_double,
-            params.upper_bound as c_double,
-            params.iter as c_int,
-            params.depth as c_int,
-            params.attc as c_int,
-            global_evaluate,
-        )
-    }
+                let userdata = Userdata {
+                    evaluate: Box::new(raw_evaluate),
+                };
 
-    T::from_vec(&out, &archetype_ctx)
+                let userdata_ptr: *const Userdata = &userdata as *const Userdata;
+
+                unsafe {
+                    raw::biteopt_optimize(
+                        out_ptr,
+                        dimension as c_int,
+                        userdata_ptr as *const c_void,
+                        params.lower_bound as c_double,
+                        params.upper_bound as c_double,
+                        params.iter as c_int,
+                        params.depth as c_int,
+                        params.attc as c_int,
+                        global_evaluate,
+                    )
+                }
+            });
+            threads.push(t);
+        }
+
+        for thread in threads.into_iter() {
+            thread.join().unwrap();
+        }
+
+        let best = best.read().unwrap();
+        best.1.clone()
+    })
+    .unwrap()
 }
 
 extern "C" fn global_evaluate(
@@ -156,6 +199,19 @@ mod tests {
         let mut params = BiteOptParameters::default();
         params.set_lower_bound(-10.0);
         params.set_upper_bound(10.0);
+        let optimized = optimize(&TwoPoly { x: 5.0, y: 6.0 }, params, |twopoly| {
+            (twopoly.x - 2.0).abs() + (twopoly.y - 8.0).abs()
+        });
+        assert!((optimized.x - 2.0).abs() < 0.00001);
+        assert!((optimized.y - 8.0).abs() < 0.00001);
+    }
+
+    #[test]
+    pub fn test_2polynomial_thready() {
+        let mut params = BiteOptParameters::default();
+        params.set_lower_bound(-10.0);
+        params.set_upper_bound(10.0);
+        params.set_portfolio_copies(50);
         let optimized = optimize(&TwoPoly { x: 5.0, y: 6.0 }, params, |twopoly| {
             (twopoly.x - 2.0).abs() + (twopoly.y - 8.0).abs()
         });
